079d53211ef502c6103e14704b84c66c9194df82
Nick Mitchell <nick@kitset.io>
2021-06-09 18:46:26 +1000

    feat: init webhook

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,9 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import fs from 'fs';
 
 export default (req, res) => {
   res.statusCode = 200;
-  res.json({ 
+  if(req.query && req.query.logs){
+    res.type('text/plain');
+    res.send(fs.readFileSync('./logs.txt', 'utf-8'));
+  }
+  const data = { 
     query: req.query, 
     body: req.body,
-  });
+  };
+  fs.writeFileSync('./logs.txt', JSON.stringify(data, null, 4), 'utf8');
+  res.json(data);
 }
